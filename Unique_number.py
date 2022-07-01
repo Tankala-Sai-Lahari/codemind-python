cfc26f8f3fc061079e69c6558d8d50dc987b2ea8
Tankala-Sai-Lahari <99111736+Tankala-Sai-Lahari@users.noreply.github.com>
2022-07-01 07:42:22 +0530

    l=list(input()) l1=[] for i in l:     if i not in l1:         l1.append(i) if len(l)==len(l1):     print("Unique Number") else:     print("Not Unique Number")

--- Unique_number.py
+++ Unique_number.py
@@ -1,9 +1,9 @@
-n=input()
-t=""
-for i in n:
-    if i not in t:
-        t+=i
-if t==n:
+l=list(input())
+l1=[]
+for i in l:
+    if i not in l1:
+        l1.append(i)
+if len(l)==len(l1):
     print("Unique Number")
 else:
     print("Not Unique Number")
