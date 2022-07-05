d4cc04db9c5a4d01d073d086a0fc2ef07dd4c81c
Tankala-Sai-Lahari <99111736+Tankala-Sai-Lahari@users.noreply.github.com>
2022-07-05 11:56:51 +0530

    n,m=map(int,input().split()) l1=list(map(int,input().split())) l2=list(map(int,input().split())) l3=[] for i in l1+l2:     if i in l1 and i in l2 and i not in l3:         l3.append(i) print(len(l3))

--- Count_elements_I.py
+++ Count_elements_I.py
@@ -1,9 +1,8 @@
-a,b=map(int,input().split())
-l=list(map(int,input().split()))
-f=list(map(int,input().split()))
-z=list(set(l+f))
-c=0
-for i in z:
-    if i in l and i in f:
-        c+=1
-print(c)
+n,m=map(int,input().split())
+l1=list(map(int,input().split()))
+l2=list(map(int,input().split()))
+l3=[]
+for i in l1+l2:
+    if i in l1 and i in l2 and i not in l3:
+        l3.append(i)
+print(len(l3))
