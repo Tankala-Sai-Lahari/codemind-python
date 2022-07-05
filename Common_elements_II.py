35c3d3503dfd765c21760b02596e1bb4a8358a98
Tankala-Sai-Lahari <99111736+Tankala-Sai-Lahari@users.noreply.github.com>
2022-07-05 11:55:05 +0530

    n,m=map(int,input().split()) l1=list(map(int,input().split())) l2=list(map(int,input().split())) l3=[] t=[] for i in l1+l2:     if i in l1 and i in l2:         l3.append(i)     if i not in t:         t.append(i) for i in t:     if i not in l3:         print(i,end=" ")

--- Common_elements_II.py
+++ Common_elements_II.py
@@ -1,11 +1,14 @@
-a,b=map(int,input().split())
-l=list(map(int,input().split()))
-f=list(map(int,input().split()))
-c=[]
-for i in l:
-    if i not in f:
-        c.append(i)
-for i in f:
-    if i not in l:
-        c.append(i)       
-print(*c)
+n,m=map(int,input().split())
+l1=list(map(int,input().split()))
+l2=list(map(int,input().split()))
+l3=[]
+t=[]
+for i in l1+l2:
+    if i in l1 and i in l2:
+        l3.append(i)
+    if i not in t:
+        t.append(i)
+for i in t:
+    if i not in l3:
+        print(i,end=" ")
+        
