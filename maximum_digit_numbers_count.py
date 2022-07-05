c29b5bbe2fbd698110dc5f4603079da0c161ad39
Tankala-Sai-Lahari <99111736+Tankala-Sai-Lahari@users.noreply.github.com>
2022-07-05 14:57:13 +0530

    n=int(input()) l=list(map(int,input().split())) l1=[] for i in l:     l1.append(len(str(i))) m=max(l1) for i in l:     if m==len(str(i)):         print(i,end=" ")

--- maximum_digit_numbers_count.py
+++ maximum_digit_numbers_count.py
@@ -2,9 +2,8 @@ n=int(input())
 l=list(map(int,input().split()))
 l1=[]
 for i in l:
-    if i not in l1:
-        l1.append(len(str(i)))
-length=max(l1)
+    l1.append(len(str(i)))
+m=max(l1)
 for i in l:
-    if len(str(i))==length:
+    if m==len(str(i)):
         print(i,end=" ")
