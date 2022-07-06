5a964a2fbe35df3308f86647d4a726eab66798d9
Tankala-Sai-Lahari <99111736+Tankala-Sai-Lahari@users.noreply.github.com>
2022-07-06 11:56:40 +0530

    s=input() w="" for i in s:     if i not in w:         w+=i print(s==w)

--- Isogram_or_not.py
+++ Isogram_or_not.py
@@ -1,9 +1,6 @@
-l=list(input().lower())
-l1=[]
-for i in l:
-    if i not in l1:
-        l1.append(i)
-if len(l)==len(l1):
-    print(True)
-else:
-    print(False)
+s=input()
+w=""
+for i in s:
+    if i not in w:
+        w+=i
+print(s==w)
