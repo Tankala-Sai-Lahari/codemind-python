423960ef1d1b02fd241bba9500f1c987c03797b6
Tankala-Sai-Lahari <99111736+Tankala-Sai-Lahari@users.noreply.github.com>
2022-07-06 11:50:10 +0530

    s=input() for i in "aeiou":     if i not in s:         print(False)         break else:     print(True)

--- All_vowels_in_a_string.py
+++ All_vowels_in_a_string.py
@@ -1,11 +1,7 @@
 s=input()
-l=[]
-for i in s:
-    if i in "AEIOUaeiou" and i not in l:
-        l.append(i)
 for i in "aeiou":
-    if i not in l:
-        print("False")
+    if i not in s:
+        print(False)
         break
 else:
-    print("True")
+    print(True)
