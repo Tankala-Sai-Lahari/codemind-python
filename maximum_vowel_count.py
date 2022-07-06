16858fddd6ff2801f7759ee675eb54c17ad4f73d
Tankala-Sai-Lahari <99111736+Tankala-Sai-Lahari@users.noreply.github.com>
2022-07-06 12:08:15 +0530

    s=input() for i in "aeiou":     if i not in s:         print(False)         break else:     print(True)

--- maximum_vowel_count.py
+++ maximum_vowel_count.py
@@ -1,9 +1,7 @@
-l=input().split()
-s=[]
-for i in l:
-    c=0
-    for j in i:
-        if j in "AEIOUaeiou" :
-            c+=1
-    s.append(c)
-print(max(s))
+s=input()
+for i in "aeiou":
+    if i not in s:
+        print(False)
+        break
+else:
+    print(True)
