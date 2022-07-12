cdd4b2dffdf4619459a2403fd7c93e494a567197
Tankala-Sai-Lahari <99111736+Tankala-Sai-Lahari@users.noreply.github.com>
2022-07-13 00:19:28 +0530

    l=list(input().lower()) t=[] for i in l:     if i not in t and l.count(i)==1 and i!=" ":         t.append(i) print(len(t))

--- count_unique_characters.py
+++ count_unique_characters.py
@@ -1,6 +1,6 @@
-s=list(input().lower())
-l=[]
-for i in s:
-    if i not in l and i!=" " and s.count(i)<=1:
-        l.append(i)
-print(len(l))
+l=list(input().lower())
+t=[]
+for i in l:
+    if i not in t and l.count(i)==1 and i!=" ":
+        t.append(i)
+print(len(t))
