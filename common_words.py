98a1b863abe7114e0becb111886ea531cb0e3c8e
Tankala-Sai-Lahari <99111736+Tankala-Sai-Lahari@users.noreply.github.com>
2022-07-13 06:41:49 +0530

    s1=input().lower().split() s2=input().lower().split() for i in s2:     if i in s1:         print(i,end=" ")

--- common_words.py
+++ common_words.py
@@ -1,9 +1,5 @@
 s1=input().lower().split()
 s2=input().lower().split()
-l=[]
-for i in s1+s2:
-    if i in s1 and i in s2 and i not in l:
-        l.append(i)
 for i in s2:
-    if i in l:
+    if i in s1:
         print(i,end=" ")
