26dcefff19b90fb3228c73323699df90664b1298
Tankala-Sai-Lahari <99111736+Tankala-Sai-Lahari@users.noreply.github.com>
2022-10-27 10:41:07 +0530

    a=int(input()) b=c=d=r=rev=0 while a>0:     b=a%10     a//=10     r=r*10+b while r>0:     c=r%10     r//=10     if d<1:         if c==6:             c=9             d=1     rev=rev*10+c print(rev)

--- Maximum_69_Number.py
+++ Maximum_69_Number.py
@@ -1,6 +1,15 @@
-n=list(input())
-for i in range(len(n)):
-    if n[i]=='6':
-        n[i]='9'
-        break
-print(int("".join(n)))
+a=int(input())
+b=c=d=r=rev=0
+while a>0:
+    b=a%10
+    a//=10
+    r=r*10+b
+while r>0:
+    c=r%10
+    r//=10
+    if d<1:
+        if c==6:
+            c=9
+            d=1
+    rev=rev*10+c
+print(rev)
