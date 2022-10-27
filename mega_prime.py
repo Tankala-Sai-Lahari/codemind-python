5fd135c50fee89ef82e53ba65784af8cf392a6e1
Tankala-Sai-Lahari <99111736+Tankala-Sai-Lahari@users.noreply.github.com>
2022-10-27 11:01:34 +0530

    def prime(a):     c=0     for i in range(1,a+1):         if a%i==0:             c+=1     if c==2:         return 1     else:         return 0 a=int(input()) if prime(a)==1:     flag=0     while a>0:         rem=a%10         if prime(rem)==1:             flag=1             a//=10         else:             flag=0             break     if flag==1:         print("Mega Prime")     else:         print("Not Mega Prime") else:     print("Not Mega Prime")

--- mega_prime.py
+++ mega_prime.py
@@ -1,21 +1,26 @@
-def prime(num):
-    if num>1:
-        for i in range(2,int(num**0.5)+1):
-            if num%i==0:
-                return False
+def prime(a):
+    c=0
+    for i in range(1,a+1):
+        if a%i==0:
+            c+=1
+    if c==2:
+        return 1
+    else:
+        return 0
+a=int(input())
+if prime(a)==1:
+    flag=0
+    while a>0:
+        rem=a%10
+        if prime(rem)==1:
+            flag=1
+            a//=10
         else:
-            return True
-    else:
-        return False
-num=int(input())
-if prime(num): 
-    while num>0:
-        if not(prime(num%10)):
-            print("Not Mega Prime")
+            flag=0
             break
-        num//=10
-    else:
+    if flag==1:
         print("Mega Prime")
+    else:
+        print("Not Mega Prime")
 else:
     print("Not Mega Prime")
-    
