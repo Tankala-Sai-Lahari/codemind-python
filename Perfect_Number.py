5c29fb545aa3efd1865a9a4633f231b2e9d1e488
Tankala-Sai-Lahari <99111736+Tankala-Sai-Lahari@users.noreply.github.com>
2022-10-27 10:31:37 +0530

    a=int(input()) sum=0 for i in range(1,a):     if (a%i==0):         sum+=i if (sum==a):     print("True") else:     print("False")

--- Perfect_Number.py
+++ Perfect_Number.py
@@ -1,9 +1,9 @@
-n=int(input())
-s=0
-for i in range(1,n//2+1):
-    if n%i==0:
-        s+=i
-if s==n:
-    print(True)
+a=int(input())
+sum=0
+for i in range(1,a):
+    if (a%i==0):
+        sum+=i
+if (sum==a):
+    print("True")
 else:
-    print(False)
+    print("False")
